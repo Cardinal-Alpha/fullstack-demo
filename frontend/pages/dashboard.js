beedc017b36effeef41f945a4b170c18aa41d215
Ryuuji <renaldi96.aldi@gmail.com>
2021-02-10 20:21:51 +0700

    Simplify HashRouter usage on dashboard page

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
 import { Ripple } from 'primereact/ripple';
-import { HashRouter, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Head from 'next/head';
 import { useRouter } from 'next/router'; 
 import { RestAPI } from '../api/rest';
@@ -17,22 +17,9 @@ class _DashboardLayout extends Component {
         super(props);
         this.api = undefined;      
         this.menu=[
-            {label: 'Contact', url: '/', template: this.menuTemplate},
-            {label: 'User', url: '/user', template: this.menuTemplate}
+            {label: 'Contact', url: '#/'},
+            {label: 'User', url: '#/user'}
         ];
-    }
-
-    menuTemplate(item, opt){
-      const icon = item.icon && <span className={opt.iconClassName}></span>;
-      const label = item.label && <span className={opt.labelClassName}>{item.label}</span>;
-      const submenuIcon = item.items && <span className={opt.submenuIconClassName}></span>;
-      const anchor = React.forwardRef( (props, ref) => (<a ref={ref} {...props} role="menuitem" className={opt.className} target={item.target} aria-haspopup={item.items != null}>
-            {icon}
-            {label}
-            {submenuIcon}
-            <Ripple />
-        </a>) );
-      return <Link to={item.url} component={anchor} />
     }
 
     render() {
@@ -48,13 +35,13 @@ class _DashboardLayout extends Component {
         }}/>;
 
         return (
-          <HashRouter basename="/" >
             <div className="full-viewport">
               <Head>
                 <title>Dashboard</title>
               </Head>
               <Menubar style={{width:'100%'}} model={this.menu} start={<div style={{width:'50px'}}></div>} end={logoutButton}/>
               <div className='content-row p-d-flex p-flex-column p-ai-center'>
+                  <HashRouter basename="/" >
                     <Switch>
                       <Route exact path='/'>
                         <ContactsTable />
@@ -63,9 +50,9 @@ class _DashboardLayout extends Component {
                         <h7>Under Construction</h7>
                       </Route>
                     </Switch>
+                  </HashRouter>
               </div>
             </div>
-          </HashRouter>
         );
       }else{
         const refresh = ()=>{
