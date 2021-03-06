73c33fc753edd967422144dd7887a66da04f004b
Ryuuji <renaldi96.aldi@gmail.com>
2021-03-06 23:47:57 +0700

    Insert urlpatterns

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,5 +1,6 @@
 from .contacts import urls as contacts_url
-from . import myaccess
+from . import myaccess, whoami
 
 urlpatterns = contacts_url.urlpatterns
 urlpatterns += myaccess.urlpatterns
+urlpatterns += whoami.urlpatterns
